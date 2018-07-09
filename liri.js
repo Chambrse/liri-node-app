155e7540da4152b2ec18ec1d46fdf929d5031e9b
Chambrse <shaneechambry@gmail.com>
2018-07-08 22:08:22 -0700

    added spotify, notes

--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
+
+/* Get all the node modules in there */
 require("dotenv").config();
 const keys = require('./keys');
 var Spotify = require('node-spotify-api');
@@ -6,14 +8,17 @@ var Twitter = require('twitter');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+
+/* Get the arguments */
 let args = process.argv.slice(2);
 
+/* figure out which command to use. */
 switch (args[0]) {
     case 'my-tweets':
         getTweets();
         break;
     case 'spotify-this-song':
-
+        spotifySearch(args[1]);
         break;
     case 'movie-this':
 
@@ -26,11 +31,13 @@ switch (args[0]) {
         break;
 }
 
+/* for formatting */
+const line = "--------------------------------------------";
+
 function getTweets() {
 
-    const line = "--------------------------------------------";
-
-    var params = { screen_name: 'realDonaldTrump', tweet_mode: 'extended' };
+    /* Twitter api call */
+    var params = { screen_name: 'chambrse', tweet_mode: 'extended' };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
 
         if (error) {
@@ -38,18 +45,47 @@ function getTweets() {
             return;
         };
 
+        /* print out the appropriate items */
         for (var i = 0; i < tweets.length; i++) {
+
             console.log(line);
-            console.log("Tweet:");
-            console.log(tweets[i].full_text);
-            console.log("");
-            console.log("TimeStamp:");
             console.log(tweets[i].created_at);
+            console.log("");
+            console.log(tweets[i].full_text);
+
         };
 
         console.log(line);
 
     });
-}
+};
+
+function spotifySearch(title) {
+
+    /* Spotify api call */
+    spotify.search({ type: 'track', query: title, limit: 1 }, function(err, data) {
+
+        if (err) {
+          return err;
+        }
+
+        console.log(line);
+
+        console.log(data.tracks.items[0].name);
+
+        console.log("");
+
+        console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+
+        console.log("Album: " + data.tracks.items[0].album.name);
+
+        console.log("Preview: " + data.tracks.items[0].preview_url);
+
+        console.log(line);
+
+
+      });
+            
+};
 
 
