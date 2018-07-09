0a5a9bba450e2d16da2d91570a0c9c80430da26b
Chambrse <shaneechambry@gmail.com>
2018-07-09 10:31:26 -0700

    added appendfile and do-what-it-says

--- liri.js
+++ liri.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const keys = require('./keys');
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
+var request = require("request");
+var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
@@ -13,23 +15,49 @@ var client = new Twitter(keys.twitter);
 let args = process.argv.slice(2);
 
 /* figure out which command to use. */
-switch (args[0]) {
-    case 'my-tweets':
-        getTweets();
-        break;
-    case 'spotify-this-song':
-        spotifySearch(args[1]);
-        break;
-    case 'movie-this':
 
-        break;
-    case 'do-what-it-says':
+liriSwitch(args);
 
-        break;
+function liriSwitch(args) {
+    switch (args[0]) {
+        case 'my-tweets':
 
-    default:
-        break;
-}
+            fs.appendFileSync("log.txt", "###" + args + "\n");
+
+            getTweets();
+            break;
+        case 'spotify-this-song':
+
+            fs.appendFileSync("log.txt", "###" + args + "\n");
+
+            if (args[1]) {
+                spotifySearch(args[1]);
+            } else {
+                spotifySearch("The sign");
+            };
+            break;
+        case 'movie-this':
+
+            fs.appendFileSync("log.txt", "###" + args + "\n");
+
+            if (args[1]) {
+                getMovie(args[1]);
+            } else {
+                getMovie("Mr. Nobody");
+            };
+            break;
+        case 'do-what-it-says':
+
+            fs.appendFileSync("log.txt", "###" + args + "\n");
+
+            readInstructions();
+
+            break;
+
+        default:
+            break;
+    };
+};
 
 /* for formatting */
 const line = "--------------------------------------------";
@@ -53,9 +81,15 @@ function getTweets() {
             console.log("");
             console.log(tweets[i].full_text);
 
+            fs.appendFileSync("log.txt", line + "\n");
+            fs.appendFileSync("log.txt", tweets[i].created_at + "\n");
+            fs.appendFileSync("log.txt", "" + "\n");
+            fs.appendFileSync("log.txt", tweets[i].full_text + "\n");
+
         };
 
         console.log(line);
+        fs.appendFileSync("log.txt", line + "\n");
 
     });
 };
@@ -63,29 +97,91 @@ function getTweets() {
 function spotifySearch(title) {
 
     /* Spotify api call */
-    spotify.search({ type: 'track', query: title, limit: 1 }, function(err, data) {
+    spotify.search({ type: 'track', query: title, limit: 1 }, function (err, data) {
 
         if (err) {
-          return err;
-        }
+            return err;
+        };
 
         console.log(line);
-
         console.log(data.tracks.items[0].name);
-
         console.log("");
-
         console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
-
         console.log("Album: " + data.tracks.items[0].album.name);
-
         console.log("Preview: " + data.tracks.items[0].preview_url);
-
         console.log(line);
 
+        fs.appendFileSync("log.txt", line + "\n");
+        fs.appendFileSync("log.txt", data.tracks.items[0].name + "\n");
+        fs.appendFileSync("log.txt", "" + "\n");
+        fs.appendFileSync("log.txt", "Artist: " + data.tracks.items[0].album.artists[0].name + "\n");
+        fs.appendFileSync("log.txt", "Album: " + data.tracks.items[0].album.name + "\n");
+        fs.appendFileSync("log.txt", "Preview: " + data.tracks.items[0].preview_url + "\n");
+        fs.appendFileSync("log.txt", line + "\n");
 
-      });
-            
+
+    });
+
 };
 
+function getMovie(title) {
+
+    let titleFormatted = title.replace(" ", "+");
+
+    request("http://www.omdbapi.com/?t=" + titleFormatted + "&y=&plot=full&apikey=trilogy", function (error, response, body) {
+
+        if (JSON.parse(body).Error) {
+            console.log("Movie not found. Check the spelling.");
+            return;
+        };
+
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
+
+
+            console.log(line);
+            console.log(JSON.parse(body).Title + " - " + JSON.parse(body).Year + " - Rated: " + JSON.parse(body).Rated);
+            console.log("IMDB: " + JSON.parse(body).Ratings[0].Value);
+            console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Metacritic: " + JSON.parse(body).Ratings[2].Value);
+            console.log("Produced in " + JSON.parse(body).Country);
+            console.log(JSON.parse(body).Language);
+            console.log(JSON.parse(body).Actors);
+            console.log(line);
+            console.log(JSON.parse(body).Plot);
+            console.log(line);
+
+            fs.appendFileSync("log.txt", line + "\n");
+            fs.appendFileSync("log.txt", JSON.parse(body).Title + " - " + JSON.parse(body).Year + " - Rated: " + JSON.parse(body).Rated + "\n");
+            fs.appendFileSync("log.txt", "IMDB: " + JSON.parse(body).Ratings[0].Value + "\n");
+            fs.appendFileSync("log.txt", "Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value + "\n");
+            fs.appendFileSync("log.txt", "Metacritic: " + JSON.parse(body).Ratings[2].Value + "\n");
+            fs.appendFileSync("log.txt", "Produced in " + JSON.parse(body).Country + "\n");
+            fs.appendFileSync("log.txt", JSON.parse(body).Language + "\n");
+            fs.appendFileSync("log.txt", JSON.parse(body).Actors + "\n");
+            fs.appendFileSync("log.txt", line + "\n");
+            fs.appendFileSync("log.txt", JSON.parse(body).Plot + "\n");
+            fs.appendFileSync("log.txt", line + "\n");
+
+
+        };
+
+    });
+};
+
+function readInstructions() {
+
+    let argsArray = [];
+
+    fs.readFile("random.txt", "utf8", function (err, body) {
+        let commands = body.split("\r\n");
+
+        for (var j = 0; j < commands.length; j++) {
+            args = commands[j].split(",");
+
+            liriSwitch(args);
+        };
+
+    });
+};
 
